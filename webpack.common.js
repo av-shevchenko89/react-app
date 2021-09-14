45334e923be1d6f75c3d1a015a6de27e781b2015
Anastasiia Shevchenko1 <anastasiia.shevchenko1@thomsonreuters.com>
2021-09-14 09:31:26 +0300

    separate to dev and prod configs

---
+++ webpack.common.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    resolve: {
+        modules: ['node_modules'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'React App',
+            template: path.join(__dirname, 'src', 'index.html'),
+            filename: 'index.html'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: path.resolve(__dirname, 'src'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    }
+};
---
+++ webpack.dev.js
@@ -0,0 +1,33 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require("path");
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        hot: true,
+        compress: true,
+        port: 3000,
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'assets',
+                    name: '[name].[ext]',
+                },
+            },
+        ],
+    },
+});
